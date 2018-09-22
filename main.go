2ae6cada9ff47a8ed300752ae65521d6785701d1
Daniel Naveda <danielnaveda88@gmail.com>
2018-09-22 18:49:16 +0200

    adding unit tests for the configuration package

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
 	"sync"
 
 	"github.com/danielnaveda/gocrawler/conf"
 	"github.com/danielnaveda/gocrawler/files"
 	"github.com/danielnaveda/gocrawler/worker"
+	yaml "gopkg.in/yaml.v2"
 )
 
 func main() {
 
 	var c conf.Conf
 
-	c.GetConf()
+	c.GetConf(`conf/craw.yaml`, ioutil.ReadFile, yaml.Unmarshal)
 
 	files.CreateDirIfNotExist(`temp-files`, os.MkdirAll, os.RemoveAll)
 
