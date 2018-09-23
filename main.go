1c2ac076b9c1fb118ff831a42a8d76ff90d432ad
Daniel Naveda <danielnaveda88@gmail.com>
2018-09-23 14:17:44 +0200

    adding parameters as config and make conf file optional

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
 	"os"
 	"sync"
@@ -11,11 +12,47 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
+type domainsFromParam []string
+
+func (d *domainsFromParam) String() string {
+	return "dummy implementation"
+}
+
+func (d *domainsFromParam) Set(value string) error {
+	*d = append(*d, value)
+	return nil
+}
+
 func main() {
+	var myDomainsFromParam domainsFromParam
 
-	var c conf.Conf
+	confFileFlag := flag.String("conf", "", "path of the configuration file")
 
-	c.GetConf(`conf/craw.yaml`, ioutil.ReadFile, yaml.Unmarshal)
+	apiFlag := flag.String("api", "", "api url")
+	flag.Var(&myDomainsFromParam, "domain", "domain to crawl")
+	workersPerDomainFlag := flag.Int("nworkers", 100, "domains separated by commas")
+	crawlersPerDomainFlag := flag.Int("ncrawlers", -1, "domains separated by commas")
+	saveFilesFlag := flag.Bool("savefile", true, "domains separated by commas")
+
+	flag.Parse()
+
+	var mydomains []string
+
+	var c = conf.Conf{
+		API:                      *apiFlag,
+		WorkersPerDomain:         *workersPerDomainFlag,
+		MaxPagesCrawledPerDomain: *crawlersPerDomainFlag,
+		SaveIntoFiles:            *saveFilesFlag,
+	}
+
+	c.GetConf(*confFileFlag, ioutil.ReadFile, yaml.Unmarshal)
+
+	mydomains = make([]string, len(myDomainsFromParam))
+
+	for index := range myDomainsFromParam {
+		mydomains[index] = myDomainsFromParam[index]
+	}
+	c.Domains = mydomains
 
 	files.CreateDirIfNotExist(`temp-files`, os.MkdirAll, os.RemoveAll)
 
